f354b1c081e298c5d259a30b38bc35252b63caa3
deyun.luo <deyun.luo@baishan.com>
2022-10-11 15:27:31 +0800

    tmp

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -6,23 +6,24 @@ local M = {}
 -- example of changing theme:
 
 M.ui = {
-  theme = "decay",
-  -- theme = "chadracula",
+	theme = "decay",
+	-- theme = "chadracula",
 }
 
-M.mappings = require "custom.mappings"
+M.mappings = require("custom.mappings")
 
-M.init = require "custom.init"
+M.init = require("custom.init")
 
-local plugin_confs = require "custom.plugins.configs"
+local plugin_confs = require("custom.plugins.configs")
 
 M.plugins = {
-  user = {
-    ["kyazdani42/nvim-tree.lua"] = plugin_confs.nvimtree,
-    ["neovim/nvim-lspconfig"] = plugin_confs.lspconfig,
-    ["jose-elias-alvarez/null-ls.nvim"] = plugin_confs.nullls,
-    ['mhinz/vim-startify'] = plugin_confs.vim_startify,
-  }
+	user = {
+		["kyazdani42/nvim-tree.lua"] = plugin_confs.nvimtree,
+		["neovim/nvim-lspconfig"] = plugin_confs.lspconfig,
+		["jose-elias-alvarez/null-ls.nvim"] = plugin_confs.nullls,
+		["mhinz/vim-startify"] = plugin_confs.vim_startify,
+		["mfussenegger/nvim-dap"] = plugin_confs.nvim_dap,
+	},
 }
 
 return M
